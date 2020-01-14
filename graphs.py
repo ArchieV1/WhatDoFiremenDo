beb13872e93881773c50615e2618815645a7da98
b9028825 <B9028825@newcastle.ac.uk>
2020-01-14 00:54:25 +0000

    Init

--- graphs.py
+++ graphs.py
@@ -1,3 +1,61 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import csv
+from collections import Counter
+import numpy as np
 
-csv_file = open("WhatDoFiremenDo.csv")
+with open("WhatDoFiremenDo.csv", "r", newline="", encoding="utf") as csv_file:
+    Dict = {}
+    count = Counter()
+    for x, row in enumerate(csv.reader(csv_file)):
+        # If time not in dict of "time:incidents" already, add it
+        # row[1][2:4] = First two in XX:YY
+        if row[1][2:4] not in Dict.keys():
+            Dict[row[1][2:4]] = []
+
+        for incident in row[2]:
+            Dict[row[1][2:4]] += incident
+
+list_of_times = []
+list_of_incident_types = []
+incident_occurrences = Counter()
+
+for time in Dict.keys():
+    list_of_times.append(time)
+
+    #Dict[time] = Dict[time].strip("\"[").strip("]\"").replace("\'", "").replace("[]", "[Unknown]").split("][")
+    #Dict[time] = Counter(Dict[time])
+    # Dict[time] (Where time = 00, 01 etc) is not a counter obj with "Incident type: Int". Incident type can be
+    # multiple incidents separated by a comma eg "'Ambulance, CRT' : Int"
+    print(Dict[time])
+    # Populate the list of incident types for use when generating graph (To be able to call all of them from the Dict)
+    for incident_type in list(Dict[time]):
+        if incident_type not in list_of_incident_types:
+            list_of_incident_types.append(incident_type)
+            incident_occurrences[incident_type] = Dict[time][incident_type]
+        else:
+            incident_occurrences[incident_type] += Dict[time][incident_type]
+print(list_of_incident_types)
+print(incident_occurrences)
+# Bar chart of incidences/time with incidences grouped by type
+plt.ylabel = "Incidents"
+plt.xlabel = "Time (24h)"
+
+# The position of the bars on the x-axis
+r = [x for x in range(0, 25)]
+
+# Generate all of the bars
+print("\n")
+
+# for incident_type in list_of_incident_types:
+#     bar_data = []
+#     for time in Dict.keys():
+#         if incident_type in list(Dict[time]):
+#             bar_data.append(Dict[time][incident_type])
+#         else:
+#             bar_data.append(0)
+#     print(incident_type)
+#     print(bar_data)
+#     plt.bar(r, bar_data, width=1)
+
+
+plt.show()
